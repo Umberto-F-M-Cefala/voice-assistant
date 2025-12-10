d034a0122b39d516756afb593e9e79785c8076a4
Umberto F. M. Cefalà <umberto.cefala@outlook.com>
2025-12-10 16:22:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,25 @@
 const CACHE_NAME = 'assistente-cache-v1';
-// Aggiungi qui i file fondamentali dell'app
+
 const urlsToCache = [
-  'index.html', // Cachea il file HTML principale
-  '/'           // Cachea la radice (spesso reindirizza a index.html)
+  './',
+  './index.html',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
-// Evento Install: Scrive i file nella cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aperta');
-        // Aggiungi i file di base dell'app shell alla cache
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
 });
 
-// Evento Fetch: Intercetta le richieste
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Se la risorsa è in cache, la restituisce (strategia Cache-First)
-        if (response) {
-          return response;
-        }
-        
-        // Altrimenti, prova a recuperarla dalla rete
-        // Questo è importante per cose come l'importazione di nuovi file .json
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
